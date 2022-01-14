9ed4ac62ac40f511184c0365b6597f1d3b53cf29
jsso16 <wjsthwls24@daum.net>
2022-01-14 14:40:03 +0900

    Edit moving.js 코드 최적화하기

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   // Check if element is scrolled into view
   function isScrolledIntoView(elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
+    const docViewTop = $(window).scrollTop();
+    const docViewBottom = docViewTop + $(window).height();
 
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
+    const elemTop = $(elem).offset().top;
+    const elemBottom = elemTop + $(elem).height();
 
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
   }
