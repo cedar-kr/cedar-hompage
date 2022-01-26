85c2ea331b6fdb130c20721bbcb0fb270a08a7af
koyuri <rmeodprp0856@naver.com>
2022-01-26 14:39:35 +0900

    header, footer 컴포넌트 생성, publicstyles 파일 생성

--- cedar-homepage/src/pages/index.js
+++ cedar-homepage/src/pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -8,11 +10,8 @@ export default function Home() {
         <meta name="description" content="(주)시더는 매장내에 정보나 광고 디스플레이를 원격 관리하는 클라우드 및 안드로이드 기반 디지털 사이니지 솔루션을 서비스하고 있는 회사입니다." />
         <link rel="icon" href="/icons/favicon.png" />
       </Head>
-      <main>
-      </main>
-      <footer>
-       
-      </footer>
+      <Header/>
+      <Footer/>
     </div>
   )
 }
