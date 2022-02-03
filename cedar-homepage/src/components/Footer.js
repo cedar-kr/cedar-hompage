15cefa83774dd6575bb620130f56fd0e64693702
jsso16 <wjsthwls24@daum.net>
2022-02-03 15:41:15 +0900

    [Footer 영역] 로고 이미지, 텍스트 사이 간격 수정

--- cedar-homepage/src/components/Footer.js
+++ cedar-homepage/src/components/Footer.js
@@ -6,6 +6,10 @@ const FooterContainer = styled.footer`
   padding: 30px 0px;
 `;
 
+const TextWrap = styled.div`
+  margin-top: 10px;
+`;
+
 const Info = styled.span`
   font-family: 'Noto Sans KR', sans-serif;
   font-size: 1.3rem;
@@ -25,14 +29,15 @@ export default function Footer(){
           width={70}
           alt="Cedar Logo"
         />
-        <br />
-        <Info>
-          대표자 정택성 / 이메일 info@cedar.kr / 
-          <br />
-          대표 전화 070.4901.6150 / 주소 서울특별시 서초구 강남대로 18길 5(양재동) 3층 
-          <br />
-          사업자등록번호 134-87-08645
-        </Info>
+        <TextWrap>
+          <Info>
+            대표자 정택성 / 이메일 info@cedar.kr / 
+            <br />
+            대표 전화 070.4901.6150 / 주소 서울특별시 서초구 강남대로 18길 5(양재동) 3층 
+            <br />
+            사업자등록번호 134-87-08645
+          </Info>
+        </TextWrap>
       </Wrapper>
     </FooterContainer>
   )
