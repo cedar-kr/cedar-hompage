3762e0b658f401a2fd2430836624ad5f8d38e106
jsso16 <wjsthwls24@daum.net>
2022-02-03 12:52:21 +0900

    공통 레이아웃 추가

---
+++ cedar-homepage/src/styles/Layout.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  width: 91.11%;
+  margin: 0 auto;
+`;
+
+export const Center = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
