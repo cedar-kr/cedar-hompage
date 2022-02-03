bb7a71baa0d2d172e94bbbacc9ed977e07954270
jsso16 <wjsthwls24@daum.net>
2022-02-03 12:52:38 +0900

    Header 영역 작업

--- cedar-homepage/src/components/Header.js
+++ cedar-homepage/src/components/Header.js
@@ -1,9 +1,24 @@
 import styled from 'styled-components';
+import Image from 'next/image'
+import { Wrapper } from '../styles/Layout'
 
-export default function Header({}){
+const HeaderContainer = styled.header`
+  padding: 20px 0px;
+  background-color: red;
+`;
+
+export default function Header(){
   return (
-    <div>
-      Header
-    </div>
+    <HeaderContainer>
+      <Wrapper>
+        <Image
+          priority
+          src="/imgs/logo_w.png"
+          height={18}
+          width={70}
+          alt="Cedar Logo"
+        />
+      </Wrapper>
+    </HeaderContainer>
   )
 }
