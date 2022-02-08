2b54cec8df3218d2e81a2f81ac5e38ee6a884a69
koyuri <rmeodprp0856@naver.com>
2022-02-08 11:06:55 +0900

    폰트 설정 오류 수정

--- cedar-homepage/src/pages/_document.js
+++ cedar-homepage/src/pages/_document.js
@@ -10,8 +10,8 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@&display=swap" rel="stylesheet" />
-					<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@&display=swap" rel="stylesheet" />
+					<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
+					<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
 				</Head>
 				<body>
 					<Main />
