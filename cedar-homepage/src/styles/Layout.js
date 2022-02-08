e2fa54fb912a8d42888bb247c01b6aa7c11b4cfb
jsso16 <wjsthwls24@daum.net>
2022-02-08 11:23:39 +0900

    공통 스타일 추가

--- cedar-homepage/src/styles/Layout.js
+++ cedar-homepage/src/styles/Layout.js
@@ -4,3 +4,9 @@ export const Wrapper = styled.div`
   width: 91.11%;
   margin: 0 auto;
 `;
+
+export const Center = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
--- cedar-homepage/src/styles/PublicStyles.js
+++ cedar-homepage/src/styles/PublicStyles.js
@@ -1,1 +1,16 @@
-export {};
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 3rem;
+  font-weight: 900;
+  line-height: 45px;
+`;
+
+export const Text = styled.p`
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 1.4rem;
+  font-weight: 400;
+  color: #000;
+  text-align: center;
+`;
