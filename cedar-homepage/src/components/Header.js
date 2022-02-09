e0989451aac8fdcd365e074c5d38d8a1dc2ae9ef
jsso16 <wjsthwls24@daum.net>
2022-02-09 16:14:14 +0900

    헤더(Header) 영역, 레퍼런스(Main) 영역, 회사 연락망(Contact) 영역 레이아웃 작업

--- cedar-homepage/src/components/Header.js
+++ cedar-homepage/src/components/Header.js
@@ -1,9 +1,45 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
+import Image from 'next/image'
+import { Title } from '../styles/PublicStyles'
+import { Wrapper } from '../styles/Layout'
 
-export default function Header({}){
+const HeaderContainer = styled.header`
+  padding: 20px 0px;
+  background: url(/imgs/bg_header.png);
+  background-position: center center;
+  height: 100vh;
+  max-height: 500px;
+`;
+
+export const HeaderTitle = styled(Title)`
+  padding-top: 61.9px;
+  color: #fff;
+`;
+
+export const Highlight = styled.div`
+  box-shadow: inset 0 -25px 0 #d74c4b;
+  width: fit-content;
+`;
+
+export default function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <HeaderContainer>
+      <Wrapper>
+      <Image
+          priority
+          src="/imgs/logo_w.png"
+          height={18}
+          width={70}
+          alt="Cedar Logo White"
+        />
+        <HeaderTitle>
+          더 나은 삶을 위한
+          <br />
+          새로운 기술,
+          <br />
+          <Highlight>주식회사 시더</Highlight>
+        </HeaderTitle>
+      </Wrapper>
+    </HeaderContainer>
   )
-}
+};
