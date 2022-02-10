e7b1480809ce8cf25099bb7c70020a6983611737
jsso16 <wjsthwls24@daum.net>
2022-02-10 10:45:59 +0900

    [CWR-18] 지도(Map) 영역 작업

--- cedar-homepage/src/components/Map.js
+++ cedar-homepage/src/components/Map.js
@@ -2,12 +2,22 @@ import styled from 'styled-components'
 
 const MapContainer = styled.div`
   height: 235px;
-  background-color: #9f9f9f;
 `;
+
 
 export default function Map() {
   return (
     <MapContainer>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3166.5215064453173!2d127.036959515309!3d37.47201807981576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca132c4ce0d31%3A0x552c6f04ec65d82a!2z7ISc7Jq47Yq567OE7IucIOyEnOy0iOq1rCDqsJXrgqjrjIDroZwxOOq4uCA1IDPsuLU!5e0!3m2!1sko!2skr!4v1639369444913!5m2!1sko!2skr"
+        name="주식회사 시더"
+        width="100%"
+        height="235px"
+        frameborder="0px"
+        allowfullscreen="false" 
+        loading="lazy"
+        sandbox="allow-scripts allow-popups"> 
+      </iframe>
     </MapContainer>
   )
 };
