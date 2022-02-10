28555207e66302ff92de441ad31c12c72b6371e8
jsso16 <wjsthwls24@daum.net>
2022-02-10 17:25:59 +0900

    레퍼런스(Main) 영역 폰트 적용

--- cedar-homepage/src/styles/global.js
+++ cedar-homepage/src/styles/global.js
@@ -1,9 +1,16 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'S-CoreDream-6Bold';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-6Bold.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
   html,
   body {
-    font-size: 10px;
+    font-size: 62.5%;
   }
 
   * {
