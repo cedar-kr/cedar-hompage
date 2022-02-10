f07405b12d697980cb8cc0e3a689f7302724bdba
koyuri <rmeodprp0856@naver.com>
2022-02-10 13:28:26 +0900

    [CWR-32] 연혁 영역 UI작업

--- cedar-homepage/src/components/History.js
+++ cedar-homepage/src/components/History.js
@@ -2,13 +2,44 @@ import styled from 'styled-components'
 import Image from 'next/image'
 import { Title, Text, LgText } from "../styles/PublicStyles"
 import { Center, Wrapper, Row } from '../styles/Layout'
+// import { history_data } from '../utils/contents'
+import React, { useEffect, useRef, useState } from 'react'
+import { chunk } from '../utils/func'
+import { history_data } from '../utils/data';
 
 const HistoryContainer = styled.section`
   padding: 30px 0px;
 `;
 
+const SlideView = styled.div`
+  display: flex;
+  flex-direction:row;
+  justify-content: space-between;
+  align-items:center;
+  min-width: 100%;
+`;
+
+const SlideCenter = styled.div`
+  height: 414px;
+  max-width:100%;
+  overflow:hidden;
+`;
+
+const SlideContainer = styled.div`
+  display:flex;
+  flex-direction:row;
+  max-width:100%;
+`;
+
+const SlideItems = styled.div`
+  display: flex;
+  flex-direction:column;
+  min-width: 100%;
+`;
+
 const TextPadding = styled(Text)`
   padding: 5px 0px;
+  text-align:center;
 `;
 
 const CenterPadding = styled(Center)`
@@ -21,73 +52,97 @@ const YearText = styled(LgText)`
   margin-bottom: 13px;
 `;
 
-const SlideCenter = styled(Center)`
-  height: 414px;
+const ButtonBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const SlideButton = styled(Image)`
+  height: 45px;
+  width: 45px;
 `;
 
 export default function History(props) {
-  const history_data = [
-    {
-      year:"2021",
-      projects:[
-        "청년친화강소기업 3년 연속 선정",
-        "미국 특허 등록",
-        "MODI GS인증 1등급 획득",
-        "세종큐비즈 키오스크 솔루션 개발"
-      ]
-    },{
-      year:"2020",
-      projects:[
-        "LGU+ 유심 키오스크 계약",
-        "김포국제공항 키오스크 솔루션 개발",
-        "판교 알파돔시티 빌딩안내 설비공사 계약"
-      ]
-    } 
-  ]
+  const data = chunk(history_data,2);
+  const TOTAL_SLIDES = data.length;  // 전체 슬라이드 개수(총 3개, 배열로 계산)
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideRef = useRef();
+
+  const NextSlide = () => {
+    if (currentSlide >= data.length-1) {
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+
+  const PrevSlide = () => {
+    if ( currentSlide == 0) {
+      setCurrentSlide(TOTAL_SLIDES-1);
+    } else {
+      setCurrentSlide(currentSlide - 1);
+    }
+  }
+
+  useEffect(() => {
+    slideRef.current.style.transition = 'all 0.5s ease-in-out';
+    slideRef.current.style.transform = `translateX(-${currentSlide}00%)`; // 백틱을 사용하여 슬라이드로 이동하는 에니메이션을 만듭니다.
+  }, [currentSlide]);
   
   return (
     <HistoryContainer>
       <Wrapper>
-        <Title>
-          시더의
-          <br />
-          즐거운 도전들을
-          <br />
-          소개합니다.
-        </Title>
-        <Row>
-          <Image
+      <Title>
+        시더의
+        <br />
+        즐거운 도전들을
+        <br />
+        소개합니다.
+      </Title>
+      <SlideView>
+        <ButtonBox>
+          <SlideButton
+            onClick={PrevSlide}
             priority
             src="/icons/h_left_arrow.png"
             height={45}
             width={45}
-            alt="left arrow"
           />
-          <SlideCenter>
-            {history_data.map((data,idx) => {
-              return (
-                <CenterPadding key={idx}> 
-                  <YearText>
-                    {data.year}
-                  </YearText>
-                  {data.projects.map((data,idx) => {
-                    return (
-                    <TextPadding key={idx}>{data}</TextPadding>
-                    )
-                  })}  
-                </CenterPadding>
-              )
-            })}
-          </SlideCenter>
-          <Image
-            priority
+        </ButtonBox>
+        <SlideCenter>
+          <SlideContainer ref={slideRef}>
+          {
+            data.map((t,idx)=>{
+              return <SlideItems key={idx}>
+                { 
+                  t.map((tt,idx)=>{
+                    return <CenterPadding key={idx}>
+                      <YearText>{tt.year}</YearText>
+                      {
+                        tt.projects.map((ttt,idx)=>{
+                          return <TextPadding key={idx}>{ttt}</TextPadding>
+                        })
+                      }
+                    </CenterPadding>
+                  })
+                }
+              </SlideItems>
+            })
+          }
+          </SlideContainer>
+        </SlideCenter>
+        <ButtonBox>
+          <SlideButton
+            onClick={NextSlide}
             src="/icons/h_right_arrow.png"
+            alt="right arrow"
             height={45}
             width={45}
-            alt="right arrow"
           />
-        </Row>
+        </ButtonBox>
+      </SlideView>
       </Wrapper>
-    </HistoryContainer>
+      </HistoryContainer>
   )
 };
