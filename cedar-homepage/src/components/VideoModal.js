5402acc124305be82adbe08ec71b065a688c1e2a
jsso16 <wjsthwls24@daum.net>
2022-02-17 13:19:30 +0900

    헤더(Header) 영역 동영상 및 버튼 화면 중앙 정렬

--- cedar-homepage/src/components/VideoModal.js
+++ cedar-homepage/src/components/VideoModal.js
@@ -10,10 +10,18 @@ const VideoModalContainer = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.75);
   z-index: 100;
+`;
+
+const VideoWrapper = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const VideoView = styled.video`
@@ -23,39 +31,40 @@ const VideoView = styled.video`
 `;
 
 const VideoClose = styled.div`
-  position: absolute;
-  bottom: 20%;
+  margin-top: 50px;
   z-index: 102;
 `;
 
 export default function VideoModal(props) {
-  const  { closeVideo}  = props;
+  const {closeVideo} = props;
 
   return (  
     <VideoModalContainer onClick={closeVideo}>
-      <VideoView 
-        onClick={(e)=> e.stopPropagation()} 
-        autoplay
-        muted
-        loop
-        playsInline
-        width="100%"
-        controls
-        preload="auto"
-      >
-        <source
-         src='./videos/promo_video.webm' 
-         type="video/webm"
-        />
-      </VideoView>
-      <VideoClose>
-        <Image 
-          src='/icons/video_close.png'
-          onClick={closeVideo}
-          width={45}
-          height={45}
-        />
-      </VideoClose>
+      <VideoWrapper>
+        <VideoView 
+          onClick={(e)=> e.stopPropagation()} 
+          autoplay
+          muted
+          loop
+          playsInline
+          width="100%"
+          controls
+          preload="auto"
+        >
+          <source
+           src='./videos/promo_video.webm' 
+           type="video/webm"
+          />
+        </VideoView>
+        <VideoClose>
+          <Image 
+            src='/icons/video_close.png'
+            onClick={closeVideo}
+            width={45}
+            height={45}
+          />
+        </VideoClose>
+      </VideoWrapper>
     </VideoModalContainer>
   )
 };
