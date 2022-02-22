c7765642bf10dae196ec8145af066bd2cf1f57b4
jsso16 <wjsthwls24@daum.net>
2022-02-22 11:09:47 +0900

    연혁(History) 영역 관련 function 파일 정리

--- cedar-homepage/src/utils/func.js
+++ cedar-homepage/src/utils/func.js
@@ -1,5 +1,5 @@
-export const chunk = ( arr, size)=>{
-  let i,j,data=[],chunk = size;
+export const chunk = (arr, size)=> {
+  let i, j, data=[], chunk = size;
   for (i = 0, j = arr.length; i < j; i += chunk) {
     data.push(arr.slice(i, i + chunk));
   }
