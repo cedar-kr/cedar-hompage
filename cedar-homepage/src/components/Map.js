538c0c20dfb96870d456bc6adbc831ab7161cf1d
koyuri <rmeodprp0856@naver.com>
2022-03-04 10:56:47 +0900

    [CWR-72] 지도영역 pc,테블릿 레이아웃 작업 완료

--- cedar-homepage/src/components/Map.js
+++ cedar-homepage/src/components/Map.js
@@ -2,6 +2,13 @@ import styled from 'styled-components'
 
 const MapContainer = styled.div`
   height: 235px;
+  ${({theme})=>theme.tablet`
+    height: 700px;
+    width:56.2%;
+  `};
+  ${({theme})=>theme.pc`
+    height: 524px;
+  `};
 `;
 
 export default function Map() {
@@ -11,7 +18,7 @@ export default function Map() {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3166.5215064453173!2d127.036959515309!3d37.47201807981576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca132c4ce0d31%3A0x552c6f04ec65d82a!2z7ISc7Jq47Yq567OE7IucIOyEnOy0iOq1rCDqsJXrgqjrjIDroZwxOOq4uCA1IDPsuLU!5e0!3m2!1sko!2skr!4v1639369444913!5m2!1sko!2skr"
         title="(주)시더 위치 정보" 
         width="100%"
-        height="235px"
+        height="100%"
         frameBorder="0"
         allowFullScreen={false}
         loading="lazy" /> 
