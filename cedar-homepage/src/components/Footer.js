c139f9a62229756addb5caf29330d120c55543ea
koyuri <rmeodprp0856@naver.com>
2022-03-04 10:56:10 +0900

    [CWR-73] 푸터 영역 테블릿, pc 레이아웃 작업 완료

--- cedar-homepage/src/components/Footer.js
+++ cedar-homepage/src/components/Footer.js
@@ -1,43 +1,91 @@
 import styled from 'styled-components'
 import Image from 'next/image'
 import { Wrapper } from '../styles/Layout'
+import { Default, Desktop, Mobile } from '../utils/media';
 
 const FooterContainer = styled.footer`
   padding: 30px 0px;
+  ${({theme})=> theme.tablet`
+    padding-top: 24px ;
+    padding-bottom: 60px;
+  `};
+  ${({theme})=> theme.pc`
+    padding-top: 70px;
+    padding-bottom: 60px;
+  `};
 `;
 
 const TextWrap = styled.div`
   margin-top: 10px;
+  ${({theme})=> theme.tablet`
+    margin-top: 20px;
+  `};
+    ${({theme})=> theme.pc`
+    margin-top: 29.4px;
+  `};
 `;
 
 const Info = styled.span`
   font-family: 'Noto Sans KR', sans-serif;
   font-size: 1.3rem;
+  font-weight: bold;
+  margin-right: 24px;
+  color:  '#333';
+  ${({theme})=> theme.mobile`
+    font-weight: 400;
+    line-height: 20px;
+  `};
+`;
+
+const CopyRight = styled.div`
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 1.3rem;
   font-weight: 400;
-  line-height: 20px;
   color: #333;
+  margin-top:10px;
 `;
 
 export default function Footer() {
   return (
     <FooterContainer>
       <Wrapper>
-        <Image
-          priority
-          src="/imgs/logo_r.png"
-          height={18}
-          width={70}
-          alt="Cedar Logo Red"
-        />
-        <TextWrap>
-          <Info>
-            대표자 정택성 / 이메일 info@cedar.kr / 
-            <br />
-            대표 전화 070.4901.6150 / 주소 서울특별시 서초구 강남대로 18길 5(양재동) 3층 
-            <br />
-            사업자등록번호 134-87-08645
-          </Info>
-        </TextWrap>
+        <Mobile>
+          <Image
+            priority
+            src="/imgs/logo_r.png"
+            height={18}
+            width={70}
+            alt="Cedar Logo Red"
+          />
+          <TextWrap>
+            <Info>
+              대표자 정택성 / 이메일 info@cedar.kr / 
+              <br />
+              대표 전화 070.4901.6150 / 주소 서울특별시 서초구 강남대로 18길 5(양재동) 3층 
+              <br />
+              사업자등록번호 134-87-08645
+            </Info>
+          </TextWrap>
+        </Mobile>
+        <Default>
+          <Image
+            priority
+            src="/imgs/logo_r.png"
+            height={27.4}
+            width={101}
+            alt="Cedar Logo Red"
+          />
+          <TextWrap>
+            <Info>대표자 정택성</Info>
+            <Info>이메일 info@cedar.kr</Info>
+            <Info>대표 전화 070.4901.6150</Info>
+            <Info>서울특별시 서초구 강남대로 18길5(양재동) 3층</Info>
+            <Info>사업자등록번호 134-87-08645</Info>
+          </TextWrap>
+          <CopyRight>
+            COPYRIGHT© CEDAR. ALL RIGHTS RESERVED.
+          </CopyRight>
+        </Default>
       </Wrapper>
     </FooterContainer>
   )
