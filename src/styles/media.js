c5257c3a394828bbf25931abd67d3683a390d9ad
jsso16 <wjsthwls24@daum.net>
2022-04-04 18:18:34 +0900

    미디어 쿼리 디바이스별 분기점 수정

--- src/styles/media.js
+++ src/styles/media.js
@@ -1,11 +1,11 @@
 import { css } from 'styled-components'
 
 const mediaQuery = {
-  fk: `(min-width: 1921px)`,
-  moniter: `(max-width: 1920px) and (min-width: 1441px)`,
-  desktop:`(max-width: 1440px) and (min-width: 1281px)`,
-  laptop: `(max-width: 1280px) and (min-width: 1025px)`,
-  tablet: `(max-width:1024px) and (min-width:768px)`,
+  fk: `(min-width: 1920px)`,
+  tv: `(min-width: 1440px) and (max-width: 1919px)`,
+  desktop:`(min-width: 1280px) and (max-width: 1439px)`,
+  laptop: `(min-width: 1024px) and (max-width: 1279px)`,
+  tablet: `(min-width:768px) and (max-width:1023px)`,
   mobile: `(max-width: 767px)`,
 }
 
