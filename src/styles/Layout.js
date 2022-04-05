bde3494b079a40d84cd16d4b5b4ed9d65ade9cc0
jsso16 <wjsthwls24@daum.net>
2022-04-05 13:10:17 +0900

    [CWR-116] 디바이스별 해상도에 따른 반응형 레이아웃 작업

---
+++ src/styles/Layout.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  width: 79.17%;
+
+  ${({ theme }) => theme.fk` 
+
+  `};
+  ${({ theme }) => theme.tv` 
+
+  `};
+  ${({ theme }) => theme.desktop` 
+  
+  `};
+  ${({ theme }) => theme.laptop` 
+
+  `};
+  ${({ theme }) => theme.tablet` 
+
+  `};
+  ${({ theme }) => theme.mobile` 
+
+  `};
+`;
