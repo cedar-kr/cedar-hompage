c21eab0ce5ef15276ec18eb07a4f054aa7e50f5b
koyuri <rmeodprp0856@naver.com>
2022-04-06 19:03:44 +0900

    [CWR-116]폰트 적용

--- src/section/index.jsx
+++ src/section/index.jsx
@@ -1,13 +1,25 @@
 import Header from './Header'
 import Business from './Business'
+import Referance from './Referance';
+import Solution from './Solutions';
+import SolutionBanner from './SolutionBanner';
 import Video from './Video'
+import History from './History';
+import Footer from './Footer';
+import SolutionPackage from './SolutionPackage';
 
 export const Section = () => {
   return (
     <>
       <Header />
       <Business />
+      <Referance />
+      <Solution />
+      <SolutionBanner />
+      <History />
+      <SolutionPackage />
       <Video />
+      <Footer />
     </>
   )
 }
