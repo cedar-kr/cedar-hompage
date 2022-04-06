6990051533f06f7d6ad8931572d850cd4eb3ae60
koyuri <rmeodprp0856@naver.com>
2022-04-06 19:00:30 +0900

    [CWR-116] 헤더영역 레이아웃 작업

--- src/section/Header.jsx
+++ src/section/Header.jsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import { Wrapper } from "../styles/PublicStyles";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import { Progress } from "../styles/Keyframes";
+import { headerData } from "../utils/data";
+import { useEffect, useState } from "react";
+import { useInterval } from "../utils/func";
 
 const HeaderLogo = styled(Wrapper)`
   height:80px;
@@ -8,22 +12,149 @@ const HeaderLogo = styled(Wrapper)`
   align-items:center;
 `;
 
+const HeaderSlide = styled.div`
+  display:flex;
+  background-image:url('/imgs/headers/header_bg.jpg');
+  height: 65.472vh;
+  padding-left: 298px ;
+  padding-right:185px;
+  align-items:center;
+  background-repeat: no-repeat;
+  background-size: 97%;
+  background-position: center center;
+`;
+const HeaderIntro = styled.div`
+  display:flex;
+  flex-direction:column;
+`;
 
-const HeaderSlide = styled(Wrapper)`
-  background-image:url('/imgs/header_bg.png');
-  height:710px;
+const HeaderText = styled.div`
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight:700;
+  font-size:7rem;
+  color:${props=> props.point ?'#2FCFBE':'#000'};
+  line-height:103.6px;
+  margin-bottom:${props=> props.point ?10:0}px;
+`;
+
+const HeaderEngText = styled.div`
+  height: 100px;
+  width: 710px;
+  margin-top:15px;
+`;
+
+const HeaderFrame = styled.div`
+`;
+
+const HeaderFrameImg = styled.div`
+  display:flex;
+  flex-direction:row;
+  justify-content:center;
+  align-items:center;
+  width:700px;
+`;
+
+const FrameLeft = keyframes`
+  0%{
+    transform:translate3d(0px, 0px,0px);
+  }50%{
+    transform:translate3d(-15px, -15px,0px);
+  }100%{
+    transform:translate3d(0px, 0px,0px);
+  }
+`;
+
+const FrameRight = keyframes`
+  0%{
+    transform:translate3d(0px, 0px,0px);
+
+  }50%{
+    transform:translate3d(15px, 15px,0px);
+  }100%{
+    transform:translate3d(0px, 0px,0px);
+  }
+`;
+
+const Frame = styled.div`
+  display:flex;
+  justify-content:${props=> props.left ?"left":"right"};
+  animation: ${props=> props.left?FrameLeft:FrameRight} 2s infinite;
+`;
+
+const ImageInfo = styled.div`
+  font-family: 'Noto Sans KR', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.5rem;
+  line-height: 22px;
+  text-align: right;
+  color: #8D8D8D;
+  margin-top:20px;
+`;
+
+
+const HeaderSlideTimer = styled.progress`
+ appearance: none;
+ width:100%;
+ border:0;
+ position:relative;
+ top:-4px;
+ height:5px;
+  ::-webkit-progress-bar{
+    background:white;
+  }
+  ::-webkit-progress-value {
+    background:#2FCFBE;
+  }
+  animation: ${Progress} 7s infinite;
 `;
 
 export default function Header(params) {
+  const [ slide, setSlide ] = useState(1);
+
+  useInterval(()=>{
+    if(slide === headerData.length){
+      setSlide(1);
+    }else{
+      setSlide(slide+1);
+    }
+  },7000)
 
   return (
     <>
-      <HeaderLogo width={79}>
+      <HeaderLogo width={80}>
         <Image src={'/icons/logo.png'} width={112} height={30}/>
       </HeaderLogo>
-      <HeaderSlide width={1860}>
-        
-      </HeaderSlide>
+        {
+          headerData.map((data,idx)=>{
+            return slide == data.id && (
+              <HeaderSlide width={97}>
+                <HeaderIntro>
+                  <HeaderText point>{data.text}</HeaderText>
+                  <HeaderText>더 쉽고 편리하게.</HeaderText>
+                  <HeaderEngText>
+                    <Image src={data.src} width={710} height={107}/>
+                  </HeaderEngText>
+                </HeaderIntro>
+                <HeaderFrame>
+                  <Frame left>
+                    <Image src={'/imgs/headers/header_frame_left.svg'} height={76} width={77}/></Frame>
+                  <HeaderFrameImg>
+                    <Image src={data.imgs[0].src} width={data.imgs[0].width} height={data.imgs[0].height}/>
+                    <Image src={data.imgs[1].src} width={data.imgs[1].width} height={data.imgs[1].height}/>
+                  </HeaderFrameImg>
+                  <Frame>
+                    <Image src={'/imgs/headers/header_frame_right.svg'} height={76} width={77}/>
+                  </Frame>
+                  <ImageInfo>{data.subs}</ImageInfo>
+                </HeaderFrame>
+              </HeaderSlide>
+            )
+          })
+        }
+      <Wrapper width={97}>
+        <HeaderSlideTimer min="1" max="100" value={100} ></HeaderSlideTimer>
+      </Wrapper>
     </>
   )
 }
