41301b5cbe926dc19a5d71010a1f325a7bc6a3c1
jsso16 <wjsthwls24@daum.net>
2022-04-14 17:29:55 +0900

    파트너사 소개(CompanyBanner) 영역 이미지 가운데 정렬

--- src/section/CompanyBanner.jsx
+++ src/section/CompanyBanner.jsx
@@ -1,30 +1,37 @@
 import Image from "next/image";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from "swiper";
+import styled, { keyframes } from 'styled-components'
 
+const CompanyBannerContainer = styled.section`
+  max-width: 1920px;
+  margin: 0 auto;
+`;
 
 export default function CompanyBanner(params) {
 
   return (
-    <Swiper
-      slidesPerView={1}
-      centeredSlides={true}
-      autoplay={{
-        delay: 3000,
-      }}
-      loop={true}
-      modules={[Autoplay]}
-      className="mySwiper"
-    >
-      <SwiperSlide>
-        <Image src={`/imgs/companyBanner/1.svg`} height={200} width={1920}/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image src={`/imgs/companyBanner/2.svg`} height={200} width={1920}/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image src={`/imgs/companyBanner/3.svg`} height={200} width={1920}/>
-      </SwiperSlide>
-    </Swiper>
+    <CompanyBannerContainer>
+      <Swiper
+        slidesPerView={1}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+        }}
+        loop={true}
+        modules={[Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Image src={`/imgs/companyBanner/1.svg`} height={200} width={1920}/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={`/imgs/companyBanner/2.svg`} height={200} width={1920}/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={`/imgs/companyBanner/3.svg`} height={200} width={1920}/>
+        </SwiperSlide>
+      </Swiper>
+    </CompanyBannerContainer>
   )
 }
