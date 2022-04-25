94ddbf30a68aac2a45183e0c67050d51ec48548f
jsso16 <wjsthwls24@daum.net>
2022-04-25 17:29:52 +0900

    파트너사 소개(CompanyBanner) 영역 모바일 버전 이미지 커밋 수정

--- src/mobileSection/CompanyBanner.jsx
+++ src/mobileSection/CompanyBanner.jsx
@@ -5,27 +5,17 @@ const CompanyBannerWrapper = styled.div`
   display:flex;
   justify-content:space-between;
   flex-direction:column;
-  height:206px;
-  padding-top: 23.57px;
-  padding-left: 16px;
-  padding-right:16px;
-`;
-
-const NewBannerWrapper = styled.div`
-  width: 100%;
+  /* height:206px; */
+  /* padding-top: 23.57px; */
+  /* padding-left: 16px; */
+  /* padding-right:16px; */
 `;
 
 export default function CompanyBanner() {
 
   return (
     <CompanyBannerWrapper>
-      <Image src={'/imgs/companyBanner/1_mobile.png'} height={33.33} width={360} alt={'Company Banner'} />
-      <Image src={'/imgs/companyBanner/2_mobile.png'} height={33.33} width={360} alt={'Company Banner'} />
-      <Image src={'/imgs/companyBanner/3_mobile.png'} height={33.33} width={360} alt={'Company Banner'} />
-      <Image src={'/imgs/companyBanner/4_mobile.png'} height={33.33} width={360} alt={'Company Banner'} />
-      <NewBannerWrapper>
-        <Image src={'/imgs/companyBanner/5_mobile.png'} height={33.33} width={192} alt={'Company Banner'} />
-      </NewBannerWrapper>
+      <Image src={'/imgs/companyBanner/mobile.png'} height={250} width={360} alt={'Company Banner'} />
     </CompanyBannerWrapper>
   )
 }
